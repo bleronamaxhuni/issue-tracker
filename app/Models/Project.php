7991fa81d51f073b452a908cb22e5de80b16b06e
bleronamaxhuni <74790174+bleronamaxhuni@users.noreply.github.com>
2026-06-22 22:16:56 +0200

    Implemented project ownership by including user_id

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,13 +6,19 @@ use Database\Factories\ProjectFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-#[Fillable(['name', 'description'])]
+#[Fillable(['user_id', 'name', 'description'])]
 class Project extends Model
 {
     /** @use HasFactory<ProjectFactory> */
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function issues(): HasMany
     {
